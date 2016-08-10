2b51728097bba48f1280609a4cded92fae5d9800
Alex Pustilnik <apustil@utexas.edu>
2016-08-10 16:31:13 -0500

    Added main.py, which is also our HELLO WORLD file! We've started!

---
+++ main.py
@@ -0,0 +1,8 @@
+# The main driver file! WOOT. Instructions: BASH~~~~ Use 'python <filename.py>' to compile. 
+
+# So we need the main driver HERE. This will keep our interface/menu runtime logic
+print "Hello WORLD!"
+
+# We should break up our menus into different functions, for cleanliness.
+
+
